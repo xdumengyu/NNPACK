6c948cc2e06dc2a28a1fbbb946add9149d398338
xdumengyu <xdumengyu@gmail.com>
2018-03-17 21:58:43 +0800

    仿写，smoketest pass, 速度不变

--- src/neon/winograd/f6x6k3x3.h
+++ src/neon/winograd/f6x6k3x3.h
@@ -7,6 +7,89 @@
 
 
 static NNP_INLINE void winograd_f6k3_input_transform_inplace(
+	float32x4_t q0[restrict static 1],
+	float32x4_t q1[restrict static 1],
+	float32x4_t q2[restrict static 1],
+	float32x4_t q3[restrict static 1],
+	float32x4_t q4[restrict static 1],
+	float32x4_t q5[restrict static 1],
+	float32x4_t q6[restrict static 1],
+	float32x4_t q7[restrict static 1])
+{
+	const float32x4_t const_0_25 = vdupq_n_f32(0.25f);
+
+	const float32x4_t const_1_25 = vdupq_n_f32(1.25f);
+
+	const float32x4_t const_4_25 = vdupq_n_f32(4.25f);
+
+	const float32x4_t const_5_25 = vdupq_n_f32(5.25f);
+
+	float32x4_t wq0 = vmlaq_f32(*q0 - *q6, *q4 - *q2, const_5_25);
+
+	float32x4_t wq7 = vmlaq_f32(*q7 - *q1, *q3 - *q5, const_5_25);
+
+
+
+
+//	const float32x4_t *q2_times_0_25 = vmulq_f32(*q2, const_0_25);
+
+//	const float32x4_t *q1_times_0_25 = vmulq_f32(*q1, const_0_25);
+
+	const float32x4_t q4_times_1_25 = vmulq_f32(*q4, const_1_25);
+
+	const float32x4_t q3_times_1_25 = vmulq_f32(*q3, const_1_25);
+
+
+	float32x4_t wq1 = vmlsq_f32(*q2 + *q6, *q4, const_4_25);
+
+	float32x4_t wq2 = vmlsq_f32(*q1 + *q5, *q3, const_4_25);
+
+	float32x4_t wq3 = *q6 - q4_times_1_25;
+
+	float32x4_t wq4 = *q5 - q3_times_1_25;
+
+	float32x4_t wq5 = *q2 - q4_times_1_25;
+
+	float32x4_t wq6 = *q1 - q3_times_1_25;
+
+	wq4 = vmlaq_f32(wq4, *q1, const_0_25);
+
+	wq6 = vmlaq_f32(wq6, *q5, const_0_25);
+
+	wq3 = vmlaq_f32(wq3, *q2, const_0_25);
+
+	wq5 = vmlaq_f32(*q6, wq5, vdupq_n_f32(4.0f));
+
+
+
+	const float32x4_t const_2 = vdupq_n_f32(2.0f);
+
+	wq4 = vmulq_f32(wq4, const_2);
+
+	wq6 = vmulq_f32(wq6, const_2);
+
+
+
+
+
+	*q0 = wq0;
+
+	*q1 = wq1 + wq2;
+
+	*q2 = wq1 - wq2;
+
+	*q3 = wq3 + wq4;
+
+	*q4 = wq3 - wq4;
+
+	*q5 = wq5 + wq6;
+
+	*q6 = wq5 - wq6;
+
+	*q7 = wq7;
+}
+/*
+static NNP_INLINE void winograd_f6k3_input_transform_inplace(
 	float32x4_t d0[restrict static 1],
 	float32x4_t d1[restrict static 1],
 	float32x4_t d2[restrict static 1],
@@ -73,9 +156,10 @@ static NNP_INLINE void winograd_f6k3_input_transform_inplace(
 	*d4 = wd3 - wd4;
 	*d5 = wd5 + wd6;
 	*d6 = wd5 - wd6;
-	//*d7 = wd7;
+	// *d7 = wd7;
 }
-
+*/
+#if 0
 static NNP_INLINE void winograd_f6k3_kernel_transform(
 	const float32x4_t g0, const float32x4_t g1, const float32x4_t g2,
 	float32x4_t transform0[restrict static 1],
@@ -151,6 +235,79 @@ static NNP_INLINE void winograd_f6k3_kernel_transform(
 	*transform7 = g2;
 }
 
+#else
+static NNP_INLINE void winograd_f6k3_kernel_transform(
+	const float32x4_t g0, const float32x4_t g1, const float32x4_t g2,
+	float32x4_t d0[restrict static 1],
+	float32x4_t d1[restrict static 1],
+	float32x4_t d2[restrict static 1],
+	float32x4_t d3[restrict static 1],
+	float32x4_t d4[restrict static 1],
+	float32x4_t d5[restrict static 1],
+	float32x4_t d6[restrict static 1],
+	float32x4_t d7[restrict static 1],
+	bool rescale_coefficients)
+{
+	const float32x4_t const_4 = vdupq_n_f32(4);
+	const float32x4_t two_g1 = g1 + g1;
+
+	float32x4_t a02 = g0 + g2;
+
+	float32x4_t w3 = vmlaq_f32(g0, g2, const_4);
+
+	float32x4_t w5  = vmlaq_f32(g2, g0, const_4);
+
+
+	//d1 = ((g0 + g2) + g1) * -2.0/9
+
+	//d2 = ((g0 + g2) - g1) * -2.0/9
+
+	*d1 = a02 + g1;
+
+	*d2 = a02 - g1;
+
+
+
+	//d3 = ((g2 * 4 + g0) + 2 * g1) * 1 / 90 = ((g0 + g2 + 2 * g1) + 3 * g2) * 1.0 / 90
+
+	//d4 = ((g2 * 4 + g0) - 2 * g1) * 1 / 90 = ((g0 + g2 - 2 * g1) + 3 * g2) * 1.0 / 90
+
+	//d5 = ((g2 + g0 * 4) + 2 * g1) * 1 / 180 = ((g0 + g2 + 2 * g1) + 3 * g0) * 1.0 / 90
+
+	//d5 = ((g2 + g0 * 4) - 2 * g1) * 1 / 180 = ((g0 + g2 - 2 * g1) + 3 * g0) * 1.0 / 90
+
+	*d3 = w3 + two_g1;
+
+	*d4 = w3 - two_g1;
+
+	*d5 = w5 + two_g1;
+
+	*d6 = w5 - two_g1;
+
+	*d0 = g0;
+	if (rescale_coefficients) {
+		
+		const float32x4_t minus2D9 = vdupq_n_f32(-2.0 / 9);
+		
+		*d1 = vmulq_f32(*d1, minus2D9);
+
+		*d2 = vmulq_f32(*d2, minus2D9);
+
+		const float32x4_t rev90 = vdupq_n_f32(1.0 / 90);
+
+		*d3 = vmulq_f32(*d3, rev90);
+
+		*d4 = vmulq_f32(*d4, rev90);
+
+		const float32x4_t rev180 = vdupq_n_f32(1.0 / 180);
+		
+		*d5 = vmulq_f32(*d5, rev180);
+
+		*d6 = vmulq_f32(*d6, rev180);
+	}
+	*d7 = g2;
+}
+#endif
 static NNP_INLINE void winograd_f6k3_output_transform_inplace(
 	float32x4_t m0[restrict static 1],
 	float32x4_t m1[restrict static 1],
